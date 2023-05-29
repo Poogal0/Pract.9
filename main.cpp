d1ae15b68894e1dcbb5b12b3bbf6ac4119ebcace
Poogal0 <golubchikov_2002@mail.ru>
2023-05-29 22:19:49 +0400

    v4 - added button Start

--- main.cpp
+++ main.cpp
@@ -1,25 +1,36 @@
 #include <QtWidgets>
-
 class MainWindow : public QMainWindow {
-	public:
-	MainWindow();
-	private:
-	QLabel *timerLabel;
+    public:
+    MainWindow();
+    void startButtonClick();
+   
+    private:
+    QLabel *timerLabel;
+    QPushButton *startButton;
+    QPushButton *stopButton;
 };
-
 MainWindow::MainWindow(){
-	setFixedSize(900,500);
-	QFont buttonFont("Courier",16,QFont::Bold);
-	setWindowTitle("Anim v0.1");
-	timerLabel= new QLabel(this);
-	timerLabel->setText("Test QLabel");
-	timerLabel->setGeometry(50,100,350,120);
-	timerLabel->setFont(buttonFont);
+    setFixedSize(900,500);
+    QFont buttonFont("Time",16,QFont::Bold);
+    setWindowTitle("Anim v0.1");
+    timerLabel= new QLabel(this);
+    timerLabel->setText("Test QLabel");
+    timerLabel->setGeometry(50,100,350,120);
+    timerLabel->setFont(buttonFont);
+    startButton = new QPushButton("Start",this);
+    startButton->setGeometry(50,220,70,30);
+    startButton->setFont(buttonFont);
+   
+   
+    connect (startButton, &QPushButton::clicked,this, &MainWindow::startButtonClick);
+    }
+void MainWindow::startButtonClick(){
+    timerLabel->setText("Start");
 }
 
+   
 int main(int argc,char *argv[]){
-	QApplication app(argc,argv);
-	MainWindow mainWin;
-	mainWin.show();
-	return app.exec();
-}
+    QApplication app(argc,argv);
+    MainWindow mainWin;
+    mainWin.show();
+    return app.exec();}
