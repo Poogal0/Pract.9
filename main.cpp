73cb679865896cb511f21265ef4efcf198295b18
Poogal0 <golubchikov_2002@mail.ru>
2023-05-29 22:16:57 +0400

    v3 - added QLabel into the app

--- main.cpp
+++ main.cpp
@@ -1,7 +1,25 @@
-#include <QTWidgets>
+#include <QtWidgets>
+
+class MainWindow : public QMainWindow {
+	public:
+	MainWindow();
+	private:
+	QLabel *timerLabel;
+};
+
+MainWindow::MainWindow(){
+	setFixedSize(900,500);
+	QFont buttonFont("Courier",16,QFont::Bold);
+	setWindowTitle("Anim v0.1");
+	timerLabel= new QLabel(this);
+	timerLabel->setText("Test QLabel");
+	timerLabel->setGeometry(50,100,350,120);
+	timerLabel->setFont(buttonFont);
+}
+
 int main(int argc,char *argv[]){
 	QApplication app(argc,argv);
-	QMainWindow mainWin;
+	MainWindow mainWin;
 	mainWin.show();
 	return app.exec();
 }
